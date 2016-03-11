232635ec65c921174bc8dc01d8a74eda20e5dfa3
Stefan <s.gencarau@gmail.com>
2016-03-11 09:29:34 +0200

    Fix null pointer exceptions

--- errorLog.js
+++ errorLog.js
@@ -15,22 +15,33 @@
             consoleErrorBackup.apply(this, arguments);
 
             var splittedError = error.split('\n');
-            var errTitle = splittedError[0];
+            var errTitle;
+            if (splittedError) {
+                errTitle = splittedError[0];
+            } else {
+                errTitle = error;
+            }
             var errPayload = error;
             var windowWidth = document.documentElement.clientWidth;
             var windowHeight = document.documentElement.clientHeight;
             var os = navigator.platform;
             var browser = navigator.userAgent;
             // line number
-            var lineColumn = splittedError[1];
-            var pattern = /[\d]+\:[\d]+/;
-            var errLineColumn = pattern.exec(lineColumn);
+
+            var lineColumn = null;
+            var errLineColumn = null;
+            if (splittedError) {
+                lineColumn = splittedError[1];
+                var pattern = /[\d]+\:[\d]+/;
+                errLineColumn = pattern.exec(lineColumn);
+            }
+
             var url = window.location.href;
 
 
             var errorObj = {
                 title: errTitle,
-                lineColumn: errLineColumn[0],
+                lineColumn: errLineColumn ? errLineColumn[0] : null,
                 windowHeight: windowHeight,
                 windowWidth: windowWidth,
                 os: os,
